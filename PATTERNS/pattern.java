676f8576c6efcfa67232ffb2a69ecff7d098b4b2
Shredhaya <shredhayagoel@gmail.com>
2025-08-12 00:03:43 +0530

    patterns in java

---
+++ PATTERNS/pattern.java
@@ -0,0 +1,18 @@
+package PATTERNS;
+import java.util.*;
+
+public class pattern {
+    public static void main (String [] args){
+         
+        int  a =5;
+        int b =55;
+          
+        for(int i = 1; i <= a; i++){
+            for(int j = 1; j<= b; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
