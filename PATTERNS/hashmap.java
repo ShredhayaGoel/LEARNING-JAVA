8626f3d9a1fdfc417b954987f950964a4cc54e93
Shredhaya <shredhayagoel@gmail.com>
2025-09-05 22:04:36 +0530

    HASHMAPS TECHNIQUES

--- PATTERNS/hashmap.java
+++ PATTERNS/hashmap.java
@@ -2,15 +2,29 @@ package PATTERNS;
 import java.util.*;
 public class hashmap {
 public static void main(String[] args) {
-    HashMap<Integer, Integer> map = new HashMap<>();
+    Scanner sc = new Scanner(System.in); 
+    System.out.println("Enter the number of key value pairs");   
+    int n =sc.nextInt();
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+    for(int i=0;i<n;i++){
+        System.out.println("Enter the key ");
+        int key = sc.nextInt();
+                    System.out.println("Enter the value ");
+        int value = sc.nextInt();
+                    map.put(key,value);
+
+    }
     map.put(1,2);
     map.put(2,3);
 System.out.println(map);
+System.out.println(map);
+
     System.out.println(map.get(1));
             System.out.println(map.get(2));
     System.out.println(map.containsKey(1));
     System.out.println(map.containsKey(3));
+    sc.close();
 
 }
- 
 }
