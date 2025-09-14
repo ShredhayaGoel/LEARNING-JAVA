830a6222f6d8a128e287acf27490b455dea95720
Shredhaya <shredhayagoel@gmail.com>
2025-09-14 23:54:43 +0530

    insertion sort

---
+++ PATTERNS/insertionsort.java
@@ -0,0 +1,15 @@
+package PATTERNS;
+
+import java.util.Scanner;
+
+public  class insertionsort {
+
+    public static void Insertionsort(){
+
+    }
+    public static void main (String [] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc
+        int a[] = new int[n];
+    }
+}
