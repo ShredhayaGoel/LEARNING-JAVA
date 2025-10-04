5e10e9dceabc9922bff0397743ff1662db7783e2
Shredhaya <shredhayagoel@gmail.com>
2025-10-05 00:08:14 +0530

    LOWER BOUND IN BINARY SEARCH

--- BinarySearch/LowerBound.java
+++ BinarySearch/LowerBound.java
@@ -22,12 +22,39 @@ public class LowerBound {
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+         while (true) {
+            System.out.println("Enter 1 to perform binary search or 0 to exit:");
+
+            int choice = sc.nextInt();
+
+             if (choice == 1) 
+             {
+        System.out.println("Enter the size of array");
+
         int n = sc.nextInt();
+
+            System.out.println("Enter the elements of array");
+
         int arr[] = new int[n];
-        for(int i =0; i <n; i++){
+
+        for(int i =0; i <n; i++)
+            {
             arr[i] = sc.nextInt();
         }
         System.out.println("lower bound is at index: " + lowerBound(arr, 3));
+    }
+      if (choice == 0) {
+                System.out.println("Exiting the program.");
+
+                break;
+            } 
+                 else if   (choice != 1)
+                  {
+                System.out.println("Invalid choice. Please enter 1 or 0.");
+                
+                continue;
+            }
+        }
         sc.close();
     }
 }
--- BinarySearch/sorted.java
+++ BinarySearch/sorted.java
@@ -22,11 +22,9 @@ public class sorted {
                 
             }
             System.out.println("Element not found");
-
             return -1;
         }
     public static void main(String [] args){
-
         Scanner sc = new Scanner(System.in);
         while (true) {
             System.out.println("Enter 1 to perform binary search or 0 to exit:");
@@ -36,12 +34,12 @@ public class sorted {
         System.out.println("Enter the size of array");
         int n = sc.nextInt();
         int arr[] = new int[n];
-        System.out.println("Enter the elements of array");  
+        System.out.println("Enter the elements of array"); 
+         
         for(int i=0;i<n;i++){
             arr[i] = sc.nextInt();
         }   
         System.out.println("Enter the element to be searched");
-
         int x = sc.nextInt();
         int result = binarySearch(arr, x);
         System.out.println(result);
@@ -49,8 +47,8 @@ public class sorted {
       if (choice == 0) {
                 System.out.println("Exiting the program.");
                 break;
-            } 
-            else if (choice != 1) {
+            } else if 
+            (choice != 1) {
                 System.out.println("Invalid choice. Please enter 1 or 0.");
                 continue;
             }
