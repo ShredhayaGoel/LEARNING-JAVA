ab2f3248e52f86a96b3f3c89f3abbb232d9cecaf
Shredhaya <shredhayagoel@gmail.com>
2026-03-07 10:10:41 +0530

    leetcode 392 issubsequence

---
+++ OOPS/object.java
@@ -0,0 +1,18 @@
+package OOPS;
+
+class Car{
+    int model;
+    String name;
+    long sale;
+    float version;
+}
+public class object {
+    public static void main(String[] args)
+{
+Car c1 = new Car();
+c1.name = "wagonr";
+c1.salary = " 566846";
+    System.out.println(c1.name);
+
+}
+}
