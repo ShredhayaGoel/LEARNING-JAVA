c72d2b7de163fa45ac35c7ad78fa391040165bc2
Shredhaya <shredhayagoel@gmail.com>
2026-06-15 22:49:45 +0530

    patterns

---
+++ ARRAYS/airth.java
@@ -0,0 +1,24 @@
+package ARRAYS;
+
+public class airth {
+    public static void main(String[] args) {
+      try{
+        int n = 10;
+        int res = n/0;
+
+}
+catch(ArithmeticException e){
+    System.out.println("you cannot divide by zero" + e);
+}
+        System.out.println("hello world");
+    }
+}
+
+
+/*
+overloading
+over writing
+types of polymorphism
+multi threading 
+
+ */
